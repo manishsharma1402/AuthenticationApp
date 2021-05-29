f8eaa59f41138b202097ad79059053616f90bac6
MANISH SHARMA <manishsharma00014@gmail.com>
2021-05-29 22:43:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,32 +20,31 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// FileUpload handle
-app.use( multer({dest: './uploads'}).single('profileimage'));
-//var upload = multer({ dest: './uploads' })
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use( multer({dest: './uploads'}).single('profileimage'));
+
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Handle session
+
 app.use(session({
 	secret: 'sonic',
 	saveUninitialized: true,
 	resave: true
 }));
 
-// Passport
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Validator
+
 app.use(expressValidator({
 	errorFormatter: function(param, msg, value){
 		var namespace = param.split('.'),
@@ -81,17 +80,15 @@ app.get('*', function(req, res, next){
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
+
+
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
